d163c579b6cd3350cd1c1212d618c102087f360c
Romantic <595159090004@dpu.ac.th>
2018-02-23 08:56:46 +0700

    style: responsive header

--- src/features/ui/components/Header.js
+++ src/features/ui/components/Header.js
@@ -9,14 +9,13 @@ import { MQTT_Disconnect } from 'Lib'
 
 const Header = ({disconnect}) => (
   <div className='header-item'>
-    <img src={logo} alt="" className='logo'/>
+    <img className='header-item__logo logo' src={logo} alt="" />
 
-    <div className='search'>
+    <div className='header-item__search search'>
       <input type='text' className='search__input' placeholder='Filter devices by name...'/>
-      <button className='search__button'></button>
     </div>
 
-    <div className='radio'>
+    <div className='header-item__radio'>
       <div className="form__radio-group">
         <input type="radio" className="form__radio-input" id="all" name="size"/>
         <label htmlFor="all" className="form__radio-label">
@@ -43,9 +42,9 @@ const Header = ({disconnect}) => (
 
     </div>
     
-    <div className='config-wrapper'>
+    <div className='header-item__setup'>
       <Link to='/connect' >
-        <div className='icon-nav' dangerouslySetInnerHTML={{ __html: configIcon }}></div>
+        <div className='icon icon__gear' dangerouslySetInnerHTML={{ __html: configIcon }}></div>
       </Link>
 
       <button className='btn btn--orange' type='submit' onClick={disconnect} >disconnect</button>
@@ -57,7 +56,7 @@ const Header = ({disconnect}) => (
 const HeaderContainer = ({ onDisconnect, location: { pathname } }) => (
   <div className='header'>
       {
-        (pathname==='/contents') ? <Header disconnect={onDisconnect} /> : <img src={logo} alt="" className='logo'/>
+        (pathname==='/contents') ? <Header disconnect={onDisconnect} /> : <img src={logo} alt="" className='header-item__logo'/>
       }
   </div>
 )
