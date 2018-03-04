fd296e8e99be8d705bde626b2e9e9a6399e30805
Romantic <595159090004@dpu.ac.th>
2018-03-05 00:00:18 +0700

    content: style card_content

--- src/features/ui/components/Content.js
+++ src/features/ui/components/Content.js
@@ -6,8 +6,12 @@ const Device = ({device}) => (
   <div className='MqttCard'>
     <div className={'MqttCard__header '+device.classCardHeader}> {device.d.myName} </div>
     <div className='MqttCard__content'>
-      <p> Name: {device.d.myName }</p>
-      <p> Mac: {device.info.mac} </p>
+      <p> <span>ip :</span> {device.info.ip }</p>
+      <p> <span>heap :</span> {device.d.heap} </p>
+      <p> <span>runtime :</span> {device.d.myName }</p>
+      <p> <span>millis :</span> {device.d.millis} </p>
+      <p> <span>prefix :</span> {device.info.prefix }</p>
+      <p> <span>mac :</span> {device.info.mac} </p>
     </div>
     <div className='MqttCard__footer'>footer</div>
   </div>
