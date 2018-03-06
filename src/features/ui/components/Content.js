ced7aed0eb7fdcfb57e7c8a96ef87c1f3f9d91bf
Romantic <595159090004@dpu.ac.th>
2018-03-06 12:23:18 +0700

    popup: add new state for popup delete component not use

--- src/features/ui/components/Content.js
+++ src/features/ui/components/Content.js
@@ -3,24 +3,6 @@ import { connect } from 'react-redux'
 import { getDevices } from '../selectors'
 import PopupContainer from './Popup'
 
-
-const Device = ({device}) => (
-  <div className='MqttCard'>
-    <div className={'MqttCard__header '+device.device_status}> {device.d.myName} </div>
-    <div className='MqttCard__content'>
-      <p> <span>ip :</span> {device.info.ip }</p>
-      <p> <span>heap :</span> {device.d.heap} </p>
-      <p> <span>runtime :</span> {device.d.myName }</p>
-      <p> <span>millis :</span> {device.d.millis} </p>
-      <p> <span>prefix :</span> {device.info.prefix }</p>
-      <p> <span>mac :</span> {device.info.mac} </p>
-    </div>
-    <div className='MqttCard__footer'>
-      <a href='#popup' className='btn-text'>More Info.</a>
-    </div>
-  </div>
-)
-
 const ContentContainer = ({devices}) => (
   <div className='contents' id='contents'>
     {
